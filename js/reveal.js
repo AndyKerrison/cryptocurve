a15914fde09fa49fee642e148ee1284c96ca2054
Alex <alenart91@gmail.com>
2017-12-29 02:42:54 -0600

    dec 29 upload

--- js/reveal.js
+++ js/reveal.js
@@ -3,16 +3,23 @@ window.sr = ScrollReveal({ reset:false, viewFactor  : 0.4,
                          });
 
 
+
+						 
+window.sr = ScrollReveal({ duration: 2000 });
+sr.reveal('.phone-icons', 100);						 
+						 
 sr.reveal('#pic1',{
   origin: 'top',
   distance: '10rem',
   duration: 900,
+  viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
 });
 
 sr.reveal('#pic2',{
   origin: 'bottom',
   distance: '10rem',
   duration: 900,
+  viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
   
 });
 
@@ -22,22 +29,23 @@ sr.reveal('#pic3',{
   distance: '10rem',
   viewOffset  : {bottom: 189},
   delay:0,
+  viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
 });
 
 sr.reveal('#pic4',{
   duration: 900,
   origin:'right',
   distance: '10rem',
-  viewOffset  : {bottom: 189},
   delay:0,
+  viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
 });
 
 sr.reveal('#pic5',{
   duration: 900,
   origin:'bottom',
   distance: '10rem',
-  viewOffset  : {bottom: 189},
   delay:0,
+  viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
 });
 
 sr.reveal('.phone-container',{
@@ -70,4 +78,177 @@ sr.reveal('.fiat-image-container',{
   origin: 'bottom',
   distance: '10rem',
   duration: 1200,
+});
+
+sr.reveal('.portfolio-info',{
+  origin: 'left',
+  distance: '10rem',
+  duration: 1200,
+  viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+});
+
+sr.reveal('.portfolio-image-container',{
+  origin: 'right',
+  distance: '10rem',
+  duration: 1200,
+  viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+});
+
+sr.reveal('#roadmap1',{
+  origin: 'left',
+  distance: '10rem',
+  duration: 1200,
+});
+
+sr.reveal('#roadmap2',{
+  origin: 'right',
+  distance: '10rem',
+  duration: 1200,
+});
+
+sr.reveal('#roadmap3',{
+  origin: 'left',
+  distance: '10rem',
+  duration: 1200,
+});
+
+sr.reveal('#roadmap4',{
+  origin: 'right',
+  distance: '10rem',
+  duration: 1200,
+});
+
+sr.reveal('#roadmap5',{
+  origin: 'left',
+  distance: '10rem',
+  duration: 1200,
+});
+
+sr.reveal('#roadmap6',{
+  origin: 'right',
+  distance: '10rem',
+  duration: 1200,
+});
+
+sr.reveal('#roadmap7',{
+  origin: 'left',
+  distance: '10rem',
+  duration: 1200,
+});
+
+sr.reveal('#roadmap8',{
+  origin: 'right',
+  distance: '10rem',
+  duration: 1200,
+});
+
+sr.reveal('#roadmap9',{
+  origin: 'left',
+  distance: '10rem',
+  duration: 1200,
+});
+
+sr.reveal('#roadmap10',{
+  origin: 'right',
+  distance: '10rem',
+  duration: 1200,
+});
+
+
+sr.reveal('.wallet-image-container',{
+  origin: 'left',
+  distance: '10rem',
+  duration: 900,
+  viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+});
+
+
+sr.reveal('#textline1',{
+  origin: 'left',
+  distance: '10rem',
+  duration: 1500,
+  viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+});
+
+sr.reveal('#textline2',{
+  origin: 'bottom',
+  distance: '10rem',
+  duration: 1500,
+  viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+});
+
+sr.reveal('#textline3',{
+  origin: 'top',
+  distance: '10rem',
+  duration: 1500,
+  viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+});
+
+sr.reveal('#textline4',{
+  origin: 'right',
+  distance: '10rem',
+  duration: 1500,
+  viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+});
+
+
+sr.reveal('.telegram-img',{
+  origin: 'top',
+  distance: '10rem',
+  duration: 1500,
+  viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+});
+
+sr.reveal('.telegram-text',{
+  origin: 'left',
+  distance: '10rem',
+  duration: 1500,
+  viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+});
+
+sr.reveal('.telegram-button',{
+  origin: 'right',
+  distance: '10rem',
+  duration: 1500,
+  viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+});
+
+
+sr.reveal('.trading-image-first-container',{
+  origin: 'left',
+  distance: '10rem',
+  duration: 1500,
+  viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+});
+
+
+sr.reveal('.trading-info',{
+  origin: 'top',
+  distance: '10rem',
+  duration: 1500,
+  viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+});
+
+
+sr.reveal('.trading-image-second-container',{
+  origin: 'right',
+  distance: '10rem',
+  duration: 1500,
+  viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+});
+
+
+sr.reveal('.nuke-image-container',{
+  origin: 'right',
+  distance: '10rem',
+  duration: 1500,
+  viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+});
+
+
+sr.reveal('.nuke-info',{
+  origin: 'left',
+  distance: '10rem',
+  duration: 1500,
+  viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
 });
